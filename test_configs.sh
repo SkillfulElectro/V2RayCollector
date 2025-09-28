2719ea441753e81169db25118a6423b202436fc5
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-09-28 23:23:33 +0330

    Update test_configs.sh

--- test_configs.sh
+++ test_configs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apt update && apt install wget zip -y
 wget https://github.com/lilendian0x00/xray-knife/releases/latest/download/Xray-knife-linux-64.zip
 unzip Xray-knife*.zip
 ./xray-knife http list -f Config/all_configs.txt
