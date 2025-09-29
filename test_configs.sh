98eaffda1f9442aeb8ed63bb034aaedbe43a31d2
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-09-29 11:32:29 +0330

    Optimization

--- test_configs.sh
+++ test_configs.sh
@@ -2,5 +2,4 @@
 
 wget https://github.com/lilendian0x00/xray-knife/releases/latest/download/Xray-knife-linux-64.zip
 unzip Xray-knife*.zip
-./xray-knife http list -f Config/all_configs.txt
-mv valid.txt Config/tested_configs.txt
+./xray-knife http list -f Config/all_configs.txt --url https://aistudio.google.com/ -o Config/tested_configs.txt
