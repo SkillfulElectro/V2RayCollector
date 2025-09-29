b3f1821013ecc6661f0fa5402f4cc74566a4d421
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-09-30 01:54:43 +0330

    Update test_configs.sh

--- test_configs.sh
+++ test_configs.sh
@@ -4,4 +4,5 @@ wget https://github.com/lilendian0x00/xray-knife/releases/latest/download/Xray-k
 unzip Xray-knife*.zip
 
 cat Config/all_configs.txt Config/tested_configs.txt > total.txt
-./xray-knife http list -f total.txt --url https://aistudio.google.com/ -o Config/tested_configs.txt
+sort -u total.txt > uniq.txt
+./xray-knife http list -f uniq.txt --url https://aistudio.google.com/ -o Config/tested_configs.txt
