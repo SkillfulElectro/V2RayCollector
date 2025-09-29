c243c7190a6ebd8566f3848ae78edd66cb3a2310
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-09-30 01:50:54 +0330

    prev valid configs are kept

--- test_configs.sh
+++ test_configs.sh
@@ -2,4 +2,6 @@
 
 wget https://github.com/lilendian0x00/xray-knife/releases/latest/download/Xray-knife-linux-64.zip
 unzip Xray-knife*.zip
-./xray-knife http list -f Config/all_configs.txt --url https://aistudio.google.com/ -o Config/tested_configs.txt
+
+cat Config/all_configs.txt Config/tested_configs.txt > total.txt
+./xray-knife http list -f total.txt --url https://aistudio.google.com/ -o Config/tested_configs.txt
